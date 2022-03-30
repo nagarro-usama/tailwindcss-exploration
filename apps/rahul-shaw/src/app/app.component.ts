71d0a888f763be4dbd445085fe4959f2a291017d
Usama Ansari <usama.ansari@nagarro.com>
2022-03-30 16:42:29 +0530

    🎉 initiated projects for Rahul, Vivek and Piyush

---
+++ apps/rahul-shaw/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tailwindcss-exploration-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rahul-shaw';
+}
